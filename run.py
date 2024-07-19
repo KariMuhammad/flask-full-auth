e4a8fba8fe7360bd67cc7a026c359c419da2fee3
Karim-Muhammad <kimoomar007@gmail.com>
2024-07-20 00:55:59 +0300

    release 2

--- run.py
+++ run.py
@@ -1,11 +1,10 @@
 # run.py
 from main import create_app
 from extensions import db
-from flask_migrate import Migrate, upgrade
+from flask_migrate import Migrate
 
 app = create_app()
 migrate = Migrate(app, db)
 
-with app.app_context():
-    db.create_all()
-    upgrade()
+if __name__ == '__main__':
+    app.run(debug=True)
